303b612bffc7e8659e3c4f70519f30c49a9048a4
Startlinker <startlinker7@gmail.com>
2025-07-30 16:08:05 +0530

    Changed frontend routes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
 // Auth routes
 app.use('/auth', authRoutes);
